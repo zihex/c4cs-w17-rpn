03fcdda150bcbb926b7a99f1b739ee1573876fbf
Zihe <zihex@umich.edu>
2017-03-10 18:33:33 -0500

    first commit

---
+++ rpn.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import operator
+
+operators = {
+    '+' : operator.add,
+    '-' : operator.sub
+    }
+
+def calculate(arg):
+    stack = list()
+    for operand in arg.split():
+        try:
+            operand = float(operand)
+            stack.append(operand)
+        except ValueError:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            stack.append(operators[operand](arg1, arg2))
+            
+    return stack.pop()
+
+def main():
+    while True:
+        result = calculate(input("rpn calc> "))
+        print('Result' , result)
+if __name__ == '__main__':
+    main()

0443efd7207533652b91a31f36b83209e5aa8918
Zihe <zihex@umich.edu>
2017-03-17 14:33:37 -0400

    test_carat added to unittest

--- test_rpn.py
+++ test_rpn.py
@@ -8,3 +8,6 @@ class TestBasics(unittest.TestCase):
     def test_substract(self):
         result = rpn.calculate("5 3 -")
         self.assertEqual(2, result)
+    def test_carat(self):
+        result = rpn.calculate("2 3 ^")
+        self.assertEqual(8, result)
